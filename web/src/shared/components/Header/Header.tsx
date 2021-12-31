9f0541d9707b587b9ec8ac1a5d7a09f26ff4b37f
DenStuk <den.stuk00@gmail.com>
2022-01-01 02:01:11 +0300

    feat(web): add header component

--- web/src/shared/components/Header/Header.tsx
+++ web/src/shared/components/Header/Header.tsx
@@ -1,23 +1,35 @@
-import React, {FC} from "react";
+import React from "react";
 import "./Header.sass";
 import FA from "react-fontawesome";
+import { Link } from "react-router-dom";
 
-export const Header: FC = () => {
+export const Header: React.FC = () => {
     const isAuthorized = true;
 
-    return <header className="page-header">
-        <div className="page-header__logo">
-            Conference<span className="page-header__color-wrapper">Hall</span>
-        </div>
-        <div className="page-header__user">
-            {
-                isAuthorized ? <div className="page-header__user-info">
-                    <div>@denstuk</div>
-                    <FA className="page-header__user-img" name="user-circle" />
-                </div> : <div>
-                    <button>Регистрация</button>
-                </div>
-            }
-        </div>
-    </header>
-}
+    return (
+        <header className="page-header">
+            <div className="page-header__logo">
+                <Link to="/">
+                    Conference<span className="page-header__color-wrapper">Hall</span>
+                </Link>
+            </div>
+            <div className="page-header__user">
+                {isAuthorized ? (
+                    <div className="page-header__user-info">
+                        <div className="self-board__user">
+                            <p className="self-board__username">Denis Stuk</p>
+                            <p className="self-board__tag">@denstuk</p>
+                        </div>
+                        <div className="self-board__photo">
+                            <FA className="page-header__user-img" name="user-circle" />
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        <button>Регистрация</button>
+                    </div>
+                )}
+            </div>
+        </header>
+    );
+};
