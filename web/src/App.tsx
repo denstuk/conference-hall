8b1fe9b2dca6f1b68ae6f71e28ea7d136351762d
DenStuk <den.stuk00@gmail.com>
2022-01-01 02:02:20 +0300

    feat(web): add react router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import {Home} from "./pages/Home/Home";
-import {Page} from "./shared/components/Page/Page";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Conference } from "./pages/Conference/Conference";
+import { Home } from "./pages/Home/Home";
+import { Page } from "./shared/components/Page/Page";
 
 function App() {
-    return <div className="App">
-        <Page content={<Home />} />
-    </div>;
+    return (
+        <div className="application">
+            <React.Fragment>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Page content={<Home />} />} />
+                        <Route path="/conferences/:id" element={<Page content={<Conference />} />} />
+                    </Routes>
+                </BrowserRouter>
+            </React.Fragment>
+        </div>
+    );
 }
 
 export default App;
