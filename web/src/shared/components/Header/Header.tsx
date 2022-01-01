633e45d0703ff8e4a6cdcceae4f386f7046fff78
DenStuk <den.stuk00@gmail.com>
2022-01-01 20:11:57 +0300

    refactor(web): simplify header component

--- web/src/shared/components/Header/Header.tsx
+++ web/src/shared/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.sass";
-import FA from "react-fontawesome";
 import { Link } from "react-router-dom";
+import { HeaderUser } from "./HeaderUser/HeaderUser";
 
 export const Header: React.FC = () => {
     const isAuthorized = true;
@@ -9,26 +9,10 @@ export const Header: React.FC = () => {
     return (
         <header className="page-header">
             <div className="page-header__logo">
-                <Link to="/">
-                    Conference<span className="page-header__color-wrapper">Hall</span>
-                </Link>
+                <Link to="/">Conference<span className="page-header__color-wrapper">Hall</span></Link>
             </div>
             <div className="page-header__user">
-                {isAuthorized ? (
-                    <div className="page-header__user-info">
-                        <div className="self-board__user">
-                            <p className="self-board__username">Denis Stuk</p>
-                            <p className="self-board__tag">@denstuk</p>
-                        </div>
-                        <div className="self-board__photo">
-                            <FA className="page-header__user-img" name="user-circle" />
-                        </div>
-                    </div>
-                ) : (
-                    <div>
-                        <button>Регистрация</button>
-                    </div>
-                )}
+                { isAuthorized ? <HeaderUser /> : <div><button>Регистрация</button></div> }
             </div>
         </header>
     );
