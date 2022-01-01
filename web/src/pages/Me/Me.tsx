0a8a2f2ec4283460666e1b1783cab464e3c444b4
DenStuk <den.stuk00@gmail.com>
2022-01-01 22:05:21 +0300

    fix(web): unused me object

--- web/src/pages/Me/Me.tsx
+++ web/src/pages/Me/Me.tsx
@@ -1,13 +1,5 @@
 import React from "react";
 
 export const Me: React.FC = () => {
-    const user = {
-        firstName: "Denis",
-        lastName: "Stuk",
-        login: "@denstuk",
-    }
-
-    return <div className="me-page">
-        
-    </div>;
+    return <div className="me-page"></div>;
 }
