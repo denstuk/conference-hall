034bdb93bcbcd075f2eeeed7cec8238d426fe87a
DenStuk <den.stuk00@gmail.com>
2022-01-01 20:05:57 +0300

    feat(add): add me component

---
+++ web/src/pages/Me/Me.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Me: React.FC = () => {
+    const user = {
+        firstName: "Denis",
+        lastName: "Stuk",
+        login: "@denstuk",
+    }
+
+    return <div className="me-page">
+        
+    </div>;
+}
