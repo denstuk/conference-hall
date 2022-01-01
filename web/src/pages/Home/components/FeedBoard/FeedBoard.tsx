d7423e235a3fadb2339797439df7e2d170df7759
DenStuk <den.stuk00@gmail.com>
2022-01-01 20:07:48 +0300

    refactor(web): replace mock data from components

--- web/src/pages/Home/components/FeedBoard/FeedBoard.tsx
+++ web/src/pages/Home/components/FeedBoard/FeedBoard.tsx
@@ -1,20 +1,12 @@
 import React from "react";
-import { IConference } from "../../../../core/conferences/types";
 import { ConferenceList } from "../../../../shared/components/ConferenceList/ConferenceList";
-import faker from "faker";
+import { ConferenceMocker } from "../../../../shared/mocks/conferences";
 
 export const FeedBoard: React.FC = () => {
-    const conferences: IConference[] = [];
-    for (let i = 0; i < 15; i++) {
-        conferences.push({ id: String(i), title: faker.random.words(10), createdAt: faker.date.past() });
-    }
-
-    return (
-        <div>
-            <div className="your-board__header">
-                <h2 className="your-board__title">Last Conferences</h2>
-            </div>
-            <ConferenceList conferences={conferences} />
+    return <div>
+        <div className="your-board__header">
+            <h2 className="your-board__title">Last Conferences</h2>
         </div>
-    );
+        <ConferenceList conferences={ConferenceMocker.many(10)} />
+    </div>;
 };
