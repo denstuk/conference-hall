4fea42507cec18538864015fa3e23b415c31f770
DenStuk <den.stuk00@gmail.com>
2022-01-01 20:09:21 +0300

    feat(web): add routes to app component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Auth } from "./pages/Auth/Auth";
 import { Conference } from "./pages/Conference/Conference";
 import { Home } from "./pages/Home/Home";
+import { Me } from "./pages/Me/Me";
 import { Page } from "./shared/components/Page/Page";
 
 function App() {
@@ -10,8 +12,11 @@ function App() {
             <React.Fragment>
                 <BrowserRouter>
                     <Routes>
-                        <Route path="/" element={<Page content={<Home />} />} />
+                        <Route index element={<Page content={<Home />} />} />
                         <Route path="/conferences/:id" element={<Page content={<Conference />} />} />
+                        <Route path="/me" element={<Page content={<Me />} />} />
+                        <Route path="/auth" element={<Auth />} />
+                        <Route path="/admin" />
                     </Routes>
                 </BrowserRouter>
             </React.Fragment>
