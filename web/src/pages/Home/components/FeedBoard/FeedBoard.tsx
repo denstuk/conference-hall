f8c3d3e5f0218494359aa8ce9263afbaaaae0672
DenStuk <den.stuk00@gmail.com>
2022-01-02 23:49:36 +0300

    refactor(web): add search field to feed-board

--- web/src/pages/Home/components/FeedBoard/FeedBoard.tsx
+++ web/src/pages/Home/components/FeedBoard/FeedBoard.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import "./FeedBoard.sass";
 import { ConferenceList } from "../../../../shared/components/ConferenceList/ConferenceList";
 import { ConferenceMocker } from "../../../../shared/mocks/conferences";
+import FA from "react-fontawesome";
 
 export const FeedBoard: React.FC = () => {
-    return <div>
+    return <div className="feed-board">
         <div className="your-board__header">
             <h2 className="your-board__title">Last Conferences</h2>
+        </div>
+        <div className="feed-board__bar">
+            <div className="feed-board__search-bar">
+                <FA name="search" />
+                <input />
+            </div>
+            <div className="feed-board__pagination">
+                <button><FA name="chevron-left" /></button>
+                <p>1</p>
+                <button><FA name="chevron-right" /></button>
+            </div>
         </div>
         <ConferenceList conferences={ConferenceMocker.many(10)} />
     </div>;
