0a0236e302461a28daa1444a265d3636594eaed6
DenStuk <den.stuk00@gmail.com>
2022-01-03 00:05:41 +0300

    feat(backend): add api, users controllers

---
+++ backend/ConferenceHall.API/Application/Http/Controllers/ApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceHall.API.Application.Http.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class ApiController : ControllerBase
+	{
+	}
+}
---
+++ backend/ConferenceHall.API/Application/Http/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using ConferenceHall.API.Domain.Dtos;
+using ConferenceHall.API.Domain.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceHall.API.Application.Http.Controllers
+{
+	public class UsersController : ApiController
+	{
+		private readonly IUserService _userService;
+		private readonly IMapper _mapper;
+
+		public UsersController(IUserService userService, IMapper mapper)
+		{
+			_userService = userService;
+			_mapper = mapper;
+		}
+
+		[HttpGet("")]
+		[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+		public async Task<ActionResult<IEnumerable<SecureUserDto>>> GetUsers()
+		{
+			var users = await _userService.GetUsersFilter();
+			return Ok(_mapper.Map<IEnumerable<SecureUserDto>>(users));
+		}
+
+		[HttpPost("")]
+		public async Task<ActionResult<SecureUserDto>> CreateUser([FromBody] SignUpDto dto)
+		{
+			var user = await _userService.CreateUser(dto);
+			return Ok(user);
+		}
+	}
+}
