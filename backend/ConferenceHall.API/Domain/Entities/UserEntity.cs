98fef0642152436599a3cf8154ed6aae6e6b66cd
DenStuk <den.stuk00@gmail.com>
2022-01-03 00:04:59 +0300

    feat(backend): add user entity

---
+++ backend/ConferenceHall.API/Domain/Entities/UserEntity.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConferenceHall.API.Domain.Entities
+{
+	[Table("users")]
+	public class UserEntity
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[Key]
+		[Column("id")]
+		public Guid Id { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		[Column("login")]
+		public string Login { get; set; }
+
+		[Required]
+		[StringLength(100)]
+		[EmailAddress]
+		[Column("email")]
+		public string Email { get; set; }
+
+		[Required]
+		[Column("password")]
+		public string Password { get; set; }
+
+		public ICollection<ConferenceEntity> Conferences { get; set; } = new List<ConferenceEntity>();
+	}
+}
