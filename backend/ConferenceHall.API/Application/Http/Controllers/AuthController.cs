4ad3d9f0e6ad641471fd678e9d42e738d07c5285
DenStuk <den.stuk00@gmail.com>
2022-01-03 15:28:20 +0300

    feat(backend): add sign-in functionality

--- backend/ConferenceHall.API/Application/Http/Controllers/AuthController.cs
+++ backend/ConferenceHall.API/Application/Http/Controllers/AuthController.cs
@@ -1,9 +1,12 @@
 ﻿using ConferenceHall.API.Application.Http.Providers;
-using ConferenceHall.API.Domain.Dtos;
+using ConferenceHall.API.Domain.Auth.Dtos;
+using ConferenceHall.API.Domain.Auth.Interfaces;
 using ConferenceHall.API.Domain.Entities;
 using ConferenceHall.API.Domain.Services.Interfaces;
 using ConferenceHall.API.Infrastructure.Database.Repositories;
 using ConferenceHall.API.Infrastructure.Database.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ConferenceHall.API.Application.Http.Controllers;
@@ -14,13 +17,20 @@ public class AuthController : ApiController
     private readonly IUserService _userService;
     private readonly IJwtService _jwtService;
     private readonly IHttpProvider _httpProvider;
+    private readonly IHashService _hashService;
 
-    public AuthController(IUserRepository userRepository, IUserService userService, IJwtService jwtService, IHttpProvider httpProvider)
+    public AuthController(
+        IUserRepository userRepository, 
+        IUserService userService, 
+        IJwtService jwtService, 
+        IHttpProvider httpProvider,
+        IHashService hashService)
     {
         _userRepository = userRepository;
         _userService = userService;
         _jwtService = jwtService;
         _httpProvider = httpProvider;
+        _hashService = hashService;
     }
     
     [HttpPost("sign-up")]
@@ -33,18 +43,26 @@ public class AuthController : ApiController
         if (checkLogin.Count > 0) return Conflict("Login already taken");
 
         UserEntity user = await _userService.CreateUser(dto);
-        string token = _jwtService.Generate(user.Id);
         
+        string token = _jwtService.Generate(user.Id);
         return Ok(token);
     }
 
     [HttpPost("sign-in")]
-    public async Task<ActionResult> SingIn([FromBody] FilterListParams filterParams)
+    public async Task<ActionResult<string>> SingIn([FromBody] SignInDto dto)
     {
-        return Ok(await _userRepository.FilterList(filterParams));
+        List<UserEntity> users = await _userRepository.FilterList(new FilterListParams() { Email = dto.Email, Take = 1 });
+        if (users.Count == 0) return NotFound("User not found");
+
+        UserEntity user = users[0];
+        if (!_hashService.TryPassword(dto.Password, user.Salt, user.Password)) return Unauthorized("Invalid password");
+        
+        string token = _jwtService.Generate(user.Id);
+        return Ok(token);
     }
 
     [HttpGet("me")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public async Task<ActionResult<UserEntity>> Me()
     {
         var user = await _httpProvider.GetUser(HttpContext);
---
+++ backend/ConferenceHall.API/Domain/Auth/Dtos/SignInDto.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace ConferenceHall.API.Domain.Auth.Dtos;
+
+public class SignInDto
+{
+    [JsonProperty("email")] 
+    public string Email { get; set; } = default!;
+
+    [JsonProperty("password")] 
+    public string Password { get; set; } = default!;
+}
