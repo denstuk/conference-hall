3437b5dd4ba0719947693eb9b436687febc57386
DenStuk <den.stuk00@gmail.com>
2022-01-05 00:49:17 +0300

    feat(backend): add filter messages method

--- backend/ConferenceHall.API/Infrastructure/Database/Repositories/Interfaces/IMessageRepository.cs
+++ backend/ConferenceHall.API/Infrastructure/Database/Repositories/Interfaces/IMessageRepository.cs
@@ -1,8 +1,9 @@
-﻿using ConferenceHall.API.Domain.Messages.Entities;
+﻿using ConferenceHall.API.Domain.Messages.Dtos;
+using ConferenceHall.API.Domain.Messages.Entities;
 
 namespace ConferenceHall.API.Infrastructure.Database.Repositories.Interfaces;
 
 public interface IMessageRepository : IBaseRepository<MessageEntity>
 {
-    
+    Task<List<MessageEntity>> FilterList(FilterMessagesDto filterDto);
 }
---
+++ backend/ConferenceHall.API/Infrastructure/Database/Repositories/MessageRepository.cs
@@ -0,0 +1,23 @@
+﻿using ConferenceHall.API.Domain.Messages.Dtos;
+using ConferenceHall.API.Domain.Messages.Entities;
+using ConferenceHall.API.Infrastructure.Database.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConferenceHall.API.Infrastructure.Database.Repositories;
+
+public class MessageRepository : BaseRepository<MessageEntity>, IMessageRepository
+{
+    public MessageRepository(DatabaseContext context) : base(context)
+    {
+    }
+
+    public async Task<List<MessageEntity>> FilterList(FilterMessagesDto filterDto)
+    {
+        var qb = _dbSet.AsQueryable();
+        if (filterDto.ConferenceId is not null) qb = qb.Where(m => m.ConferenceId == filterDto.ConferenceId);
+        qb = qb.OrderByDescending(m => m.CreatedAt);
+        qb = qb.Include(m => m.Creator);
+        qb = qb.Include(m => m.Conference);
+        return await qb.ToListAsync();
+    }
+}
