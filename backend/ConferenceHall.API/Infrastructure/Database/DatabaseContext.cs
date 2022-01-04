e809275ea0594e18d77bbfea3c230b631642868d
DenStuk <den.stuk00@gmail.com>
2022-01-05 00:19:50 +0300

    feat(backend): add relation for message in context

--- backend/ConferenceHall.API/Infrastructure/Database/DatabaseContext.cs
+++ backend/ConferenceHall.API/Infrastructure/Database/DatabaseContext.cs
@@ -1,4 +1,6 @@
-﻿using ConferenceHall.API.Domain.Users.Entities;
+﻿using ConferenceHall.API.Domain.Conferences.Entities;
+using ConferenceHall.API.Domain.Messages.Entities;
+using ConferenceHall.API.Domain.Users.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace ConferenceHall.API.Infrastructure.Database
@@ -13,6 +15,21 @@ namespace ConferenceHall.API.Infrastructure.Database
 				.HasMany(p => p.Conferences)
 				.WithMany(c => c.Users)
 				.UsingEntity(j => j.ToTable("user_conference"));
+			
+			modelBuilder.Entity<ConferenceEntity>()
+				.HasOne(u => u.Creator)
+				.WithMany(u => u.CreatedConferences)
+				.HasForeignKey(k => k.CreatorId);
+
+			modelBuilder.Entity<MessageEntity>()
+				.HasOne(m => m.Creator)
+				.WithMany(u => u.Messages)
+				.HasForeignKey(m => m.CreatorId);
+
+			modelBuilder.Entity<MessageEntity>()
+				.HasOne(m => m.Conference)
+				.WithMany(c => c.Messages)
+				.HasForeignKey(m => m.ConferenceId);
 		}
 	}
 }
