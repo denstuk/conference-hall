b6a9c5feab67e36dc1acf5e79e02b49dc307c39c
DenStuk <den.stuk00@gmail.com>
2022-01-04 22:38:54 +0300

    feat(web): add simple add message logic

--- web/src/pages/Conference/Conference.tsx
+++ web/src/pages/Conference/Conference.tsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Conference.sass";
 import faker from "faker";
 import { Message } from "./components/Message/Message";
-import { MessageMocker } from "../../shared/mocks/messages";
+import { MessageMocker } from "../../shared/lib/mocks/messages";
 
 export const Conference: React.FC = () => {
     // const { id } = useParams(); TODO:Development
-    const title: string = faker.random.words(5);
+    const [title] = useState(faker.random.words(5));
+    const [messages, setMessages] = useState(MessageMocker.many(2));
+    const [inputMessage, setInputMessage] = useState("");
 
-    return <div className="conference-page">
-        <div className="conference-page__title">
-            <p>{title}</p>
+    const sendMessage = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+        e.preventDefault();
+        setMessages([
+            {
+                id: "newmessage" + String(messages.length + 1),
+                text: inputMessage,
+                createdAt: new Date(),
+                user: {
+                    id: String(1),
+                    login: "denstuk",
+                    email: "denstuk@example.com",
+                    role: 2,
+                    createdAt: new Date(),
+                },
+            },
+            ...messages,
+        ]);
+    };
+
+    return (
+        <div className="conference-page">
+            <div className="conference-page__title">
+                <p>{title}</p>
+            </div>
+            <div className="conference-page__messages">
+                {messages.map((m) => (
+                    <Message key={m.id} message={m} />
+                ))}
+            </div>
+            <form onSubmit={(e) => sendMessage(e)} className="conference-page__input">
+                <input name="message" onChange={(e) => setInputMessage(e.target.value)} />
+            </form>
         </div>
-        <div className='conference-page__messages'>
-            { MessageMocker.many(50).map((m) => <Message message={m} />) }
-        </div>
-        <input className="conference-page__input" />
-    </div>;
+    );
 };
