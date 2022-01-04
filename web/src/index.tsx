0f2e1aba79e18fcebed265085f022e5ab1a3b4bf
DenStuk <den.stuk00@gmail.com>
2022-01-04 22:31:54 +0300

    feat(web): add toaster container

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./shared/styles/index.sass";
+import 'react-toastify/dist/ReactToastify.css';
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./shared/store";
+import {ToastContainer} from "react-toastify";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+            <ToastContainer />
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
