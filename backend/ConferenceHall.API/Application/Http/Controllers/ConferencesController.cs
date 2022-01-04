288760eec552e8d657238e2df148c285645ae5d5
DenStuk <den.stuk00@gmail.com>
2022-01-05 00:21:24 +0300

    feat(backend): add conference controller

--- backend/ConferenceHall.API/Application/Http/Controllers/ConferencesController.cs
+++ backend/ConferenceHall.API/Application/Http/Controllers/ConferencesController.cs
@@ -1,23 +1,22 @@
 ﻿using ConferenceHall.API.Domain.Conferences.Entities;
-using ConferenceHall.API.Infrastructure.Database.Repositories.Interfaces;
+using ConferenceHall.API.Domain.Conferences.Handlers.Queries;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ConferenceHall.API.Application.Http.Controllers;
 
 public class ConferencesController : ApiController
 {
-    private readonly IConferenceRepository _conferenceRepository;
-
-    public ConferencesController(IConferenceRepository conferenceRepository)
-    {
-        _conferenceRepository = conferenceRepository;
-    }
+    private readonly IMediator _mediator;
+    public ConferencesController(IMediator mediator) => _mediator = mediator;
     
-    
-    [HttpGet("")]
-    public async Task<ActionResult<IEnumerable<ConferenceEntity>>> GetConferences()
+    /// <summary>
+    /// Search conferences
+    /// </summary>
+    [HttpPost("search")]
+    public async Task<ActionResult<IEnumerable<ConferenceEntity>>> SearchConferences([FromBody] SearchConferencesQuery query)
     {
-        var conferences = await _conferenceRepository.Get();
+        var conferences = await _mediator.Send(query);
         return Ok(conferences);
     }
 }
