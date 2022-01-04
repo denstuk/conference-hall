9d5027cbddbc5b490b8e773e2f2fcb3b55a319a4
DenStuk <den.stuk00@gmail.com>
2022-01-04 22:39:24 +0300

    refactor(web): style

--- web/src/pages/Conference/components/Message/Message.tsx
+++ web/src/pages/Conference/components/Message/Message.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./Message.sass";
 import dayjs from "dayjs";
-import { IMessage } from "../../../../core/types";
+import {IMessage} from "../../../../core";
 
-type MessageProps = {
-    message: IMessage;
-}
+type MessageProps = { message: IMessage };
 
 export const Message: React.FC<MessageProps> = ({ message }: MessageProps) => {
-    return <div className="message">
-        <p className="message__login">@{message.user.login}</p>
-        <p className="message__text">{message.text}</p>
-        <div className="message__date">
-            <p>{dayjs(message.createdAt).format("HH:mm D-MMM-YYYY")}</p>
+    return (
+        <div className="message">
+            <p className="message__login">@{message.user.login}</p>
+            <p className="message__text">{message.text}</p>
+            <div className="message__date">
+                <p>{dayjs(message.createdAt).format("HH:mm D-MMM-YYYY")}</p>
+            </div>
         </div>
-    </div>;
-}
+    );
+};
--- web/src/pages/Home/components/FeedBoard/FeedBoard.tsx
+++ web/src/pages/Home/components/FeedBoard/FeedBoard.tsx
@@ -1,25 +1,31 @@
 import React from "react";
 import "./FeedBoard.sass";
 import { ConferenceList } from "../../../../shared/components/ConferenceList/ConferenceList";
-import { ConferenceMocker } from "../../../../shared/mocks/conferences";
 import FA from "react-fontawesome";
+import { ConferenceMocker } from "../../../../shared/lib/mocks/conferences";
 
 export const FeedBoard: React.FC = () => {
-    return <div className="feed-board">
-        <div className="your-board__header">
-            <h2 className="your-board__title">Last Conferences</h2>
-        </div>
-        <div className="feed-board__bar">
-            <div className="feed-board__search-bar">
-                <FA name="search" />
-                <input />
+    return (
+        <div className="feed-board">
+            <div className="your-board__header">
+                <h2 className="your-board__title">Last Conferences</h2>
             </div>
-            <div className="feed-board__pagination">
-                <button><FA name="chevron-left" /></button>
-                <p>1</p>
-                <button><FA name="chevron-right" /></button>
+            <div className="feed-board__bar">
+                <div className="feed-board__search-bar">
+                    <FA name="search" />
+                    <input />
+                </div>
+                <div className="feed-board__pagination">
+                    <button>
+                        <FA name="chevron-left" />
+                    </button>
+                    <p>1</p>
+                    <button>
+                        <FA name="chevron-right" />
+                    </button>
+                </div>
             </div>
+            <ConferenceList conferences={ConferenceMocker.many(10)} />
         </div>
-        <ConferenceList conferences={ConferenceMocker.many(10)} />
-    </div>;
+    );
 };
