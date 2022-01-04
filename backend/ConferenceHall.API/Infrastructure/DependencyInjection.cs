692cdf7ebbf8ff24b854103bb455b8175dc79c1e
DenStuk <den.stuk00@gmail.com>
2022-01-05 00:24:03 +0300

    feat(backend): register conference service

--- backend/ConferenceHall.API/Infrastructure/DependencyInjection.cs
+++ backend/ConferenceHall.API/Infrastructure/DependencyInjection.cs
@@ -1,8 +1,10 @@
 ﻿using ConferenceHall.API.Application.Http.Providers;
 using ConferenceHall.API.Domain.Auth;
 using ConferenceHall.API.Domain.Auth.Interfaces;
-using ConferenceHall.API.Domain.FileSystem.Interfaces;
-using ConferenceHall.API.Domain.Services;
+using ConferenceHall.API.Domain.Conferences.Interfaces;
+using ConferenceHall.API.Domain.Conferences.Services;
+using ConferenceHall.API.Domain.Files.Interfaces;
+using ConferenceHall.API.Domain.Files.Services;
 using ConferenceHall.API.Domain.Users.Interfaces;
 using ConferenceHall.API.Domain.Users.Services;
 using ConferenceHall.API.Infrastructure.Database.Repositories;
@@ -21,6 +23,7 @@ namespace ConferenceHall.API.Infrastructure
 			services.AddScoped<IFileService, FileService>();
 			services.AddScoped<IJwtService, JwtService>();
 			services.AddScoped<IHttpProvider, HttpProvider>();
+			services.AddScoped<IConferenceService, ConferenceService>();
 		}
 	}
 }
