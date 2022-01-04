f32ba16355e23980f1bfdbaa0fbbd3b3d7f52d66
DenStuk <den.stuk00@gmail.com>
2022-01-04 04:16:58 +0300

    refactor(web): remove username from header

--- web/src/shared/components/Header/HeaderUser/HeaderUser.tsx
+++ web/src/shared/components/Header/HeaderUser/HeaderUser.tsx
@@ -5,10 +5,6 @@ import { Link } from "react-router-dom";
 
 export const HeaderUser: React.FC = () => {
     return <Link className="header-user" to="/me">
-        <div className="header-user__username">
-            <p className="header-user__name">Denis Stuk</p>
-            <p className="header-user__login">@denstuk</p>
-        </div>
         <div className="header-user__img">
             <FA className="page-header__user-img" name="user-circle" />
         </div>
