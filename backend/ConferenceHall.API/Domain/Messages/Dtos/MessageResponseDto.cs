e99d58934b222f6192698561ce9012b14576a6f4
DenStuk <den.stuk00@gmail.com>
2022-01-05 14:11:40 +0300

    refactor(backend): add conferenceId field to messageResponseDto

--- backend/ConferenceHall.API/Domain/Messages/Dtos/MessageResponseDto.cs
+++ backend/ConferenceHall.API/Domain/Messages/Dtos/MessageResponseDto.cs
@@ -7,5 +7,6 @@ public class MessageResponseDto
     public Guid Id { get; set; }
     public string Text { get; set; } = default!;
     public UserResponseDto Creator { get; set; } = default!;
+    public Guid ConferenceId { get; set; }
     public DateTime CreatedAt { get; set; }
 }
