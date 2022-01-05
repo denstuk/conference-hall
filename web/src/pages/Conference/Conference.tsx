3095aa9736dcd995167852824f1131c2adfef038
DenStuk <den.stuk00@gmail.com>
2022-01-05 14:20:30 +0300

    feat(web): add webSockets connection

--- web/src/pages/Conference/Conference.tsx
+++ web/src/pages/Conference/Conference.tsx
@@ -1,41 +1,54 @@
 import React, {useEffect, useState} from "react";
 import "./Conference.sass";
 import faker from "faker";
-import { Message } from "./components/Message/Message";
-import { MessageMocker } from "../../shared/lib/mocks/messages";
-import {useNavigate} from "react-router-dom";
+import {Message} from "./components/Message/Message";
+import {MessageMocker} from "../../shared/lib/mocks/messages";
+import {useNavigate, useParams} from "react-router-dom";
 import {useAppSelector} from "../../shared/store";
+import {HttpTransportType, HubConnection, HubConnectionBuilder, LogLevel} from "@microsoft/signalr";
+import {IMessage} from "../../core";
+import {LocalStorage} from "../../shared/lib/providers/local-storage";
+import {StorageKey} from "../../core/constants";
 
 export const Conference: React.FC = () => {
-    const navigate = useNavigate();
-    const state = useAppSelector((state) => state.authReducer);
-
-    useEffect(() => {
-        !state.authorized && navigate("/auth?sign-in")
-    });
-
-    // const { id } = useParams(); TODO:Development
+    const [connection, setConnection] = useState<HubConnection>();
     const [title] = useState(faker.random.words(5));
     const [messages, setMessages] = useState(MessageMocker.many(2));
     const [inputMessage, setInputMessage] = useState("");
 
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const state = useAppSelector((state) => state.authReducer);
+
+    useEffect(() => {
+        if (!state.authorized) {
+            navigate("/auth?sign-in")
+        }
+        if (!connection) joinRoom();
+    });
+
+    const joinRoom = async () => {
+        const connection = new HubConnectionBuilder()
+            .withUrl("https://localhost:44391/chatHub", { skipNegotiation: true, transport: HttpTransportType.WebSockets })
+            .configureLogging(LogLevel.Information)
+            .build();
+        connection.on("ReceiveMessage", (message: IMessage) => {
+            console.log(message)
+            setMessages(messages => [message, ...messages]);
+        });
+        await connection.start();
+        await connection.invoke("Join", {
+            ConferenceId: id,
+            Token: LocalStorage.get(StorageKey.AccessToken),
+        });
+        setConnection(connection);
+    }
+
     const sendMessage = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
         e.preventDefault();
-        setMessages([
-            {
-                id: "newmessage" + String(messages.length + 1),
-                text: inputMessage,
-                createdAt: new Date(),
-                user: {
-                    id: String(1),
-                    login: "denstuk",
-                    email: "denstuk@example.com",
-                    role: 2,
-                    createdAt: new Date(),
-                },
-            },
-            ...messages,
-        ]);
+        if (connection) {
+            await connection.invoke("SendMessage", { Text: inputMessage });
+        }
     };
 
     return (
