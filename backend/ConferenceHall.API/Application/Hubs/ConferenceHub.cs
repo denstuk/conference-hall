56bb38cd3c8fe8ca5ad866deb1f4e026d84ad076
DenStuk <den.stuk00@gmail.com>
2022-01-05 13:54:35 +0300

    refactor(backend): change namings

--- backend/ConferenceHall.API/Application/Hubs/ConferenceHub.cs
+++ backend/ConferenceHall.API/Application/Hubs/ConferenceHub.cs
@@ -38,28 +38,27 @@ public class ConferenceHub : Hub
 
     public async Task SendMessage(SendMessageDto sendMessageDto)
     {
-        if (_connections.TryGetValue(Context.ConnectionId, out ConnectionDto connection))
+        if (_connections.TryGetValue(Context.ConnectionId, out ConnectionDto? connection))
         {
             var user = await ExtractUserFromToken(connection.Token);
             var conferences = await _conferenceService.GetConferencesFilter(new FilterConferenceParams()
-                { Ids = new List<Guid>() { Guid.Empty } });
+                { Ids = new List<Guid>() { Guid.Parse(connection.ConferenceId) } });
             if (conferences.Count == 0)
             {
                 throw new Exception("Conference not found");
             }
 
             var message = await _messageService.CreateMessage(new CreateMessageDto() { Text = sendMessageDto.Text }, user, conferences[0]);
-            await Clients.Group(connection.ConferenceId.ToString())
-                .SendAsync("ReceiveMessage", _mapper.Map<ResponseMessageDto>(message));
+            await Clients.Group(connection.ConferenceId)
+                .SendAsync("ReceiveMessage", _mapper.Map<MessageResponseDto>(message));
         }
     }
     
     public async Task Join(ConnectionDto dto)
     {
-        await Groups.AddToGroupAsync(Context.ConnectionId, dto.ConferenceId.ToString());
+        await Groups.AddToGroupAsync(Context.ConnectionId, dto.ConferenceId);
         _connections[Context.ConnectionId] = dto;
         await base.OnConnectedAsync();
-        //await Clients.Group(dto.ConferenceId.ToString()).SendAsync($"LogConnection - {dto.Token}");
     }
 
     private async Task<UserEntity> ExtractUserFromToken(string token)
@@ -80,7 +79,7 @@ public class ConferenceHub : Hub
 public class ConnectionDto
 {
     public string Token { get; set; } = default!;
-    public string ConferenceId { get; set; }
+    public string ConferenceId { get; set; } = default!;
 }
 
 public class SendMessageDto
