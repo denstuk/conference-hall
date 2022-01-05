8c2d7c6c84e5b7342f099a6a9904cb1ffd633c46
DenStuk <den.stuk00@gmail.com>
2022-01-05 06:10:44 +0300

    feat(backend): add UpdateMessageTableName migration

--- backend/ConferenceHall.API/Infrastructure/Database/Migrations/20220105023501_UpdateMessageTableName.Designer.cs
+++ backend/ConferenceHall.API/Infrastructure/Database/Migrations/20220105023501_UpdateMessageTableName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ConferenceHall.API.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ConferenceHall.API.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220105023501_UpdateMessageTableName")]
+    partial class UpdateMessageTableName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
