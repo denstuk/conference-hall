c5a136fccedb2ef971f5a18dc2e85de378458d45
DenStuk <den.stuk00@gmail.com>
2022-01-05 14:21:03 +0300

    feat(web): add loader, simple pagination feedBoard

--- web/src/pages/Home/components/FeedBoard/FeedBoard.tsx
+++ web/src/pages/Home/components/FeedBoard/FeedBoard.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./FeedBoard.sass";
 import { ConferenceList } from "../../../../shared/components/ConferenceList/ConferenceList";
 import FA from "react-fontawesome";
-import { ConferenceMocker } from "../../../../shared/lib/mocks/conferences";
+import {ConferencesAPI} from "../../../../shared/api/conferences";
+import {IConference} from "../../../../core";
+import TailSpin from "react-loader-spinner";
+
+const DEFAULT_STEP = 5;
 
 export const FeedBoard: React.FC = () => {
+    const [loading, setLoading] = useState(true);
+    const [conferences, setConferences] = useState<IConference[]>([]);
+    const [page, setPage] = useState(0);
+
+    useEffect(() => {
+       const fetchConferences = async () => {
+           const fetched = await ConferencesAPI.search();
+           setConferences([...fetched]);
+           setTimeout(() => setLoading(false), 1000)
+       }
+       fetchConferences().then();
+    }, []);
+
+    const incrementPage = () => {
+        if (!conferences) return;
+        const max = Math.floor(conferences.length / DEFAULT_STEP) + (conferences.length % DEFAULT_STEP === 0 ? -1 : 0);
+        return setPage(Math.min(page + 1, max));
+    }
+    const decrementPage = () => {
+        if (!conferences) return;
+        return setPage(Math.max(0, page - 1));
+    }
+    const paginate = (conferences: IConference[]) => {
+        const start = page * DEFAULT_STEP;
+        return conferences.slice(start, start + DEFAULT_STEP);
+    }
+
     return (
         <div className="feed-board">
             <div className="your-board__header">
@@ -15,17 +46,22 @@ export const FeedBoard: React.FC = () => {
                     <FA name="search" />
                     <input />
                 </div>
-                <div className="feed-board__pagination">
-                    <button>
-                        <FA name="chevron-left" />
-                    </button>
-                    <p>1</p>
-                    <button>
-                        <FA name="chevron-right" />
-                    </button>
-                </div>
+
             </div>
-            <ConferenceList conferences={ConferenceMocker.many(10)} />
+            {
+                loading
+                    ? <div className="feed-board__loader"><TailSpin type="TailSpin" color="#fb8310" /></div>
+                    : <ConferenceList conferences={paginate(conferences)} />
+            }
+            <div className="feed-board__pagination">
+                <button onClick={() => decrementPage()}>
+                    <FA name="chevron-left" />
+                </button>
+                <p>{page + 1}</p>
+                <button onClick={() => incrementPage()}>
+                    <FA name="chevron-right" />
+                </button>
+            </div>
         </div>
     );
 };
