6f913dea9b24c5d395f4a41e6c1a4a478a2f07fe
DenStuk <den.stuk00@gmail.com>
2022-01-05 06:13:28 +0300

    feat(backend): add create conference route

--- backend/ConferenceHall.API/Application/Http/Controllers/ConferencesController.cs
+++ backend/ConferenceHall.API/Application/Http/Controllers/ConferencesController.cs
@@ -1,6 +1,10 @@
-﻿using ConferenceHall.API.Domain.Conferences.Entities;
+﻿using ConferenceHall.API.Application.Http.Providers;
+using ConferenceHall.API.Domain.Conferences.Dtos;
+using ConferenceHall.API.Domain.Conferences.Handlers.Commands;
 using ConferenceHall.API.Domain.Conferences.Handlers.Queries;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ConferenceHall.API.Application.Http.Controllers;
@@ -8,15 +12,30 @@ namespace ConferenceHall.API.Application.Http.Controllers;
 public class ConferencesController : ApiController
 {
     private readonly IMediator _mediator;
-    public ConferencesController(IMediator mediator) => _mediator = mediator;
+    private readonly IHttpProvider _provider;
+
+    public ConferencesController(IMediator mediator, IHttpProvider provider)
+    {
+        _mediator = mediator;
+        _provider = provider;
+    }
     
     /// <summary>
     /// Search conferences
     /// </summary>
     [HttpPost("search")]
-    public async Task<ActionResult<IEnumerable<ConferenceEntity>>> SearchConferences([FromBody] SearchConferencesQuery query)
+    public async Task<ActionResult<IEnumerable<ResponseConferenceDto>>> SearchConferences([FromBody] SearchConferencesQuery query)
     {
         var conferences = await _mediator.Send(query);
         return Ok(conferences);
+    }
+
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [HttpPost("")]
+    public async Task<ActionResult<ResponseConferenceDto>> CreateConference([FromBody] CreateConferenceDto dto)
+    {
+        var user = await _provider.GetUser(HttpContext);
+        var conference = await _mediator.Send(new CreateConferenceCommand() { CreateDto = dto, Creator = user, });
+        return Ok(conference);
     }
 }
