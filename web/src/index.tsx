33a9db9c5b86957c4b994ed80475a43d48cadad3
DenStuk <den.stuk00@gmail.com>
2022-01-05 14:16:52 +0300

    feat(web): add loader styles

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./shared/styles/index.sass";
 import 'react-toastify/dist/ReactToastify.css';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./shared/store";
