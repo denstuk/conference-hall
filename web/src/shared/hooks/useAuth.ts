2e5761046902dab04f7b503a515d8a5b36d992e9
DenStuk <den.stuk00@gmail.com>
2022-01-06 18:51:21 +0300

    fix(web): add user to useAuth

--- web/src/shared/hooks/useAuth.ts
+++ web/src/shared/hooks/useAuth.ts
@@ -5,10 +5,14 @@ import { LocalStorage } from "../lib/providers/local-storage";
 import { StorageKey } from "../../core/constants";
 import { AuthAPI } from "../api";
 import { Toaster } from "../lib/providers/toaster";
+import {useState} from "react";
+import {IUser} from "../../core";
 
 export const useAuth = () => {
+    const [user, setUser] = useState<IUser>();
     const dispatch = useDispatch();
     const { authorize, logout } = bindActionCreators(authDispatchers, dispatch);
+
     const auth = async function checkAuthorization(): Promise<void> {
         const token = LocalStorage.get(StorageKey.AccessToken);
         if (!token) {
@@ -18,6 +22,7 @@ export const useAuth = () => {
         try {
             const user = await AuthAPI.me();
             if (user) {
+                setUser(user);
                 authorize(user);
                 return;
             }
@@ -26,5 +31,6 @@ export const useAuth = () => {
         }
         logout();
     };
-    return { auth };
+
+    return { auth, user };
 };
