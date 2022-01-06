6b92453c1d87ef0f7d16162c5258ac445f15eb5e
DenStuk <den.stuk00@gmail.com>
2022-01-07 01:33:04 +0300

    feat(backend): add createdAt field to ConferenceResponseDto

--- backend/ConferenceHall.API/Domain/Conferences/Dtos/ConferenceResponseDto.cs
+++ backend/ConferenceHall.API/Domain/Conferences/Dtos/ConferenceResponseDto.cs
@@ -8,5 +8,6 @@ public class ConferenceResponseDto
     public Guid Id { get; set; }
     public string Title { get; set; } = default!;
     public UserResponseDto Creator { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
     public List<MessageEntity> Messages { get; set; } = new List<MessageEntity>();
 }
